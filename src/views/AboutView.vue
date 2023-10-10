697e76c35221efd5b7d33bf253bc1a4b4ff58790
zackcarlson014 <jzcarlson014@gmail.com>
2023-10-10 17:41:07 -0500

    removed unused components, updated AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <button
+      class="counter-button"
+      @click="counterStore.increment">
+      {{ counterStore.count }}
+    </button>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from '@/stores/counter';
+
+const counterStore = useCounterStore();
+</script>
+
+<style>
+  .counter-button {
+    font-size: 50px;
+  }
+</style>
